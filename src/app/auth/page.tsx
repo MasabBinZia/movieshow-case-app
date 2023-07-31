e2c198f5812f0b3d4e949fbd6834c5650755689d
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-08-01 00:28:30 +0500

    02

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,34 +1,36 @@
-import React from 'react'
+import React from "react";
+import Input from "@/components/Input";
 
 const auth = () => {
-    return (
-        <div className='relative 
+  return (
+    <div
+      className="relative 
         h-full w-full 
         bg-[url(/images/hero.jpg)] 
         bg-no-repeat
         bg-center 
         bg-fixed
         bg-cover
-        '>
-            <div className='bg-black w-full h-full lg:bg-opacity-50'>
-                <nav className='px-12 py-5'>
-                    <img src='/images/logo.png' alt='logo'
-                        className='h-12 '
-                    />
-                    <div className='flex justify-center'>
-                        <div className='bg-black bg-opacity-70 px-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
-
-                        </div>
-
-                    </div>
-
-                </nav>
-{/* 14:42 */}
+        "
+    >
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12 " />
+          <div className="flex justify-center">
+            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+              <h2 className="text-white text-4xl mb-8 font-semibold">
+                Sign in
+              </h2>
+              <div className="flex flex-col gap-4">
+              <Input />
+              </div>
             </div>
+          </div>
+          {/* 16:55 */}
+        </nav>
+      </div>
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default auth
+export default auth;
---
+++ src/components/Input.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+   <Input/>
+    )
+}
+
+export default Input
